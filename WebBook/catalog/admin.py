326594662a1b0796f8538ccf18a457d426ba51ce
aka-Fenix <andruha-71@rambler.ru>
2022-09-15 09:31:55 +0700

    Первая версия приложения

---
+++ WebBook/catalog/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from .models import Author,Book,Genre,Language,Status,BookInstance
+
+#admin.site.register(Author)
+@admin.register (Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display=('last_name','first_name','date_of_birth','date_of_death')
+    fields=['first_name','last_name',
+            ('date_of_birth','date_of_death')]
+
+#admin.site.register(Book)
+class BooksInstanceInline(admin.TabularInline):
+    model=BookInstance
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'language', 'display_author')
+    inlines=[BooksInstanceInline]
+    list_filter = ('genre', 'author')
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Status)
+#admin.site.register(BookInstance)
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'inv_nom')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
+
+
+
+
+# Register your models here.
